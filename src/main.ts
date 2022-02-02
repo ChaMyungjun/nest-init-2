7fc6c5b26dfa277f7e9da9b62e22d9e0bba73d0b
ChaMyungjun <acg70159@gmail.com>
2022-02-02 21:16:40 +0900

    [UPDATE] Update nest js swagger setting init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 (async function () {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle("tkendi")
+    .setDescription("nest js postgresql init")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(8000);
 })();
 // bootstrap();
